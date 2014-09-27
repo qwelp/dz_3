257e33f437488075dcf70c8f4e9db844da04387c
wowua_ <bpm_05@mail.ru>
2014-09-27 18:34:58 +0400

    folder edit

--- _makeups/_common/_head.php
+++ _makeups/_common/_head.php
@@ -10,10 +10,7 @@
 	<title><?php echo $title ?></title>
 
 	<meta name="viewport" content="width=device-width">
-
-	<link rel="stylesheet" href="/css/normalize.css">
 	<link rel="stylesheet" type="text/css" href="/css/main.css" media="screen, projection, print">
-
 	<script src="/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
 </head>
 <body>
